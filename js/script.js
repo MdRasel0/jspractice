e622bf45f613fe5d96c9ab778904d12a7526acd8
MdRasel0 <52493009+MdRasel0@users.noreply.github.com>
2020-08-30 14:53:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function textChange(){
+	document.getElementById('p').innerHTML = "Text Changed";
+}
+function textChange2(){
+	document.getElementById('p').innerHTML = "Lorem ipsum dolor, sit amet.";
+}
